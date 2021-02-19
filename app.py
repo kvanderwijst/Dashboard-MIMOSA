20f5f0b41c6a4d08452fcaba1af844cf44dffb5a
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2021-02-19 07:55:30 +0100

    Moved back from app to index

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from common.dash import *
-from pages import plot
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.FLATLY]
@@ -7,22 +6,3 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # server = app.server
 app.config.suppress_callback_exceptions = True
-
-app.layout = dbc.Container([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content'),
-    dcc.Store(id='common-lastmodified-store')
-], className="p-5", fluid=True)  
-
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    return plot.layout
-    # if pathname == '/settings':
-    #     return settings.layout
-    # else:
-    #     return main.layout
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
