64ff649813479686d7098a7e9cbba9e73a1453ca
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2021-02-25 12:01:40 +0100

    Rename GDP file

--- pages/components/tabs.py
+++ pages/components/tabs.py
@@ -4,7 +4,7 @@ Simple dash Tabs for each part of the visualisation
 from common.dash import dcc, dbc
 
 from pages.components.tabs_components import (
-    gdp2,
+    gdp,
     costs,
     damages,
     emissions,
@@ -19,7 +19,7 @@ layout = dbc.Row(
                     [
                         dcc.Tab(label="Emissions", children=emissions.layout),
                         dcc.Tab(label="Costs", children=costs.layout),
-                        dcc.Tab(label="GDP", children=gdp2.layout),
+                        dcc.Tab(label="GDP", children=gdp.layout),
                         dcc.Tab(label="Damages", children=damages.layout),
                         dcc.Tab(label="Input parameters", children=inputparams.layout),
                         dcc.Tab(label="IPOPT output", children="tab-content-6"),
